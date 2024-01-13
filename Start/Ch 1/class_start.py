2d5cc11305417faf037a4968e77da764bebbf099
Mayur Lohana <mayurlohana121@gmail.com>
2024-01-13 14:53:31 +0000

    finished Chapters 1 and 2 of this course with hands on implementation.

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -4,26 +4,43 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
-
+    BOOK_TYPES = ('HARDCOVER', 'PAPERBACK', 'EBOOK')
     # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
 
     # TODO: create a class method
+    @classmethod
+    def get_book_types(self):
+        return self.BOOK_TYPES
 
     # TODO: create a static method
+    def get_book_list():
+        if Book.__booklist is None:
+            Book.__booklist = []
+        return Book.__booklist
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid booktype")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
-
+print('Book Types: ', Book.get_book_types())
 
 # TODO: Create some book instances
-
+book1 = Book("The Rich Dad, Poor Dad", "HARDCOVER")
+book2 = Book('Atomic Habits', "PAPERBACK")
 
 # TODO: Use the static method to access a singleton object
+books = Book.get_book_list()
+books.append(book1)
+books.append(book2)
+print(books)
--- Start/Ch 2/challenge.py
+++ Start/Ch 2/challenge.py
@@ -12,14 +12,33 @@
 # For stocks: "Ticker: Company -- $Price"
 # For bonds: "description: duration'yr' : $price : yieldamt%"
 
-class Asset():
-    pass
+from abc import ABC, abstractmethod
+class Asset(ABC):
+    def __init__(self, price) -> None:
+        self.price = price
 
-class Stock():
-    pass
+    @abstractmethod
+    def get_description(self):
+        pass
 
-class Bond():
-    pass
+class Stock(Asset):
+    def __init__(self, ticker, price, company) -> None:
+        super().__init__(price)
+        self.ticker = ticker
+        self.company = company
+
+    def get_description(self):
+        return f'{self.ticker}: {self.company} -- ${self.price}'
+
+class Bond(Asset):
+    def __init__(self, price, description, duration, yieldamt) -> None:
+        super().__init__(price)
+        self.description = description
+        self.duration = duration
+        self.yieldamt = yieldamt
+
+    def get_description(self):
+        return f'{self.description}: {self.duration}yr : ${self.price} : {self.yieldamt}%'
 
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
